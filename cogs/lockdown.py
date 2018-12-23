785ea868da07cb305c2b35e84d6e4f97afc94134
Ave Ozkal <ave@ave.zone>
2018-12-23 22:15:44 +0300

    lockdown: cleanup and fixes? :)

--- cogs/lockdown.py
+++ cogs/lockdown.py
@@ -3,6 +3,7 @@ import asyncio
 from discord.ext import commands
 from config import log_channel, staff_role_ids, participant_role, community_channels, general_channels, hacker_role, community_role
 
+
 class Lockdown:
     "Lockdown Commands"
 
@@ -12,20 +13,22 @@ class Lockdown:
     @commands.has_permissions(manage_messages=True)
     @commands.command(pass_context=True)
     async def lock(self, ctx):
-        "Locks the channel"
+        """Locks the channel"""
+        log_chan = self.bot.get_channel(log_channel)
         if ctx.message.channel in community_channels:
             roles = (hacker_role, community_role)
         else:
             roles = participant_role
         overwrites = ctx.message.channel.overwrites_for(roles[0])
-        if overwrites.send_message == False:
-            await ctx.send("The Channel is already locked!")
+        if not overwrites.send_message:
+            await ctx.send("This channel is already locked!")
             return
         overwrites.send_message = False
         overwrites.add_reactions = False
-        await asyncio.gather(*(self.bot.edit_channel_permissions(ctx.message.channel, role, overwrites) for role in roles))
+        await asyncio.gather(*(self.bot.edit_channel_permissions(ctx.channel, role, overwrites) for role in roles))
         await ctx.send("🔒 Channel locked down. Only staff members may speak. Do not bring the topic to other channels or risk disciplinary actions.")
-        ctx.send_message(modlog_channel, "Channel Lockdown for {channel} by {user}".format(ctx.message.channel.mention, ctx.message.author.mention))
+        await log_chan.send(f"Channel Lockdown for {ctx.channel.mention} by {ctx.author.mention}")
+
 
 def setup(bot):
     bot.add_cog(Lockdown(bot))
