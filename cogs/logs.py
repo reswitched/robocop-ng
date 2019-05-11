17aa5d4758da2af8ff52b0a60e6720eb8d54c78d
tomGER <25822956+tumGER@users.noreply.github.com>
2019-05-11 14:36:20 +0200

    Update logs.py

--- cogs/logs.py
+++ cogs/logs.py
@@ -20,9 +20,8 @@ class Logs(Cog):
         self.name_re = re.compile(r"[a-zA-Z0-9].*")
         self.clean_re = re.compile(r'[^a-zA-Z0-9_ ]+', re.UNICODE)
         # All lower case, no spaces, nothing non-alphanumeric
-        self.susp_words = ["sx", "tx", "reinx",  # piracy-enabling cfws
-                           "tinfoil", "dz",  # title managers
-                           "goldleaf", "lithium",  # title managers
+        self.susp_words = ["tinfoil", "dz",  # title managers
+                           "blawar", "lithium",  # title managers
                            "xci"]  # "backup" format
         susp_hellgex = "|".join([r"\W*".join(list(word)) for
                                  word in self.susp_words])
