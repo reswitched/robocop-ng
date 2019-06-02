0a85413bf55283dd95c18c075b2342d123e3522f
WerWolv <werwolv98@gmail.com>
2019-06-02 20:43:15 +0200

    Less eye cancer for the general public

--- cogs/meme.py
+++ cogs/meme.py
@@ -5,6 +5,7 @@ from discord.ext.commands import Cog
 import math
 import platform
 from helpers.checks import check_if_staff_or_ot
+from helpers.checks import check_if_staff
 
 
 class Meme(Cog):
@@ -126,7 +127,8 @@ class Meme(Cog):
     async def frolics(self, ctx):
         """test"""
         await ctx.send("https://www.youtube.com/watch?v=VmarNEsjpDI")
-            
+    
+    @commands.check(check_if_staff)
     @commands.command(hidden=True, aliases=['bs', "biracy", ":b:iracy", "🅱iracy"])
     async def batches(self, ctx):
         """Yeet"""
